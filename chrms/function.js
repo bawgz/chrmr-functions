eecba9d53c339ed61a398d3f2cafaafa136e753f
Luke Boggs <lukeboggs7@gmail.com>
2020-11-06 17:25:07 -0500

    add basic filter functionality

--- chrms/function.js
+++ chrms/function.js
@@ -8,10 +8,18 @@ const mongodb = require('mongodb');
  */
 exports.getChrms = (req, res) => {
   mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, client) {
-      
+    const filter = {};
+
+    if (req.query.filter != null && req.query.filter !== "") {
+      const filterTerms = req.query.filter.toLowerCase().trim().split(" ");
+      filter.keywords = {
+        "$in": filterTerms
+      }
+    }
+
     const col = client.db('chrmr').collection('chrms');
           
-    col.find({}).toArray(function(err, items) {
+    col.find(filter).toArray(function(err, items) {
       client.close();
       res.status(200).send(items);
     });
