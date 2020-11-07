45dd4c29d5bee65f86e2fa308cba681d067b1860
Luke Boggs <lukeboggs7@gmail.com>
2020-11-07 12:20:14 -0500

    add files function code

---
+++ files/function.js
@@ -0,0 +1,36 @@
+const { Storage } = require('@google-cloud/storage');
+
+let storage;
+const bucketName = 'chrms';
+
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.getFile = async (req, res) => {
+
+  console.log(req.params);
+
+  if (!storage) {
+    storage = new Storage();
+  }
+
+  // Downloads the file
+  try {
+    const file = await storage
+      .bucket(bucketName)
+      .file(req.params[0]);
+    const exists = await file.exists();
+
+    if (!exists[0]) {
+      return res.status(404).send("not found");
+    }
+
+    return file.createReadStream().pipe(res);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Error");
+  }
+};
