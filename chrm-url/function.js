968b8657ee22d1942861e21e85cb087e105e0f1f
Luke Boggs <lukeboggs7@gmail.com>
2021-05-23 01:52:34 -0400

    add functionality to create html page if not existing

--- chrm-url/function.js
+++ chrm-url/function.js
@@ -1,4 +1,5 @@
 const { Storage } = require('@google-cloud/storage');
+const { Readable } = require('stream');
 
 let storage;
 const bucketName = 'chrms';
@@ -16,11 +17,65 @@ exports.getChrmUrl = async (req, res) => {
 
   // Downloads the file
   try {
-    const file = await storage.bucket(bucketName).file(req.params[0]);
+    console.log(req.body);
+    const fileName = req.body.id + '.html';
+    const file = await storage.bucket(bucketName).file(fileName);
     const exists = await file.exists();
 
     if (!exists[0]) {
-      return res.status(404).send('not found');
+      const chunks = [];
+      let fileString = '';
+      const newUrl = 'https://storage.googleapis.com/chrms/' + fileName;
+      const templateFile = await storage
+        .bucket(bucketName)
+        .file('template.html');
+      await templateFile.copy(fileName, function (
+        err,
+        copiedFile,
+        apiResponse,
+      ) {
+        console.log('err');
+        console.log(err);
+        console.log('copiedFile');
+        console.log(copiedFile);
+        console.log('apiResponse');
+        console.log(apiResponse);
+        copiedFile
+          .createReadStream()
+          .on('data', function (chunk) {
+            chunks.push(chunk);
+          })
+          .on('end', function () {
+            // The file is fully downloaded.
+            console.log('end');
+            console.log(chunks);
+            fileString = Buffer.concat(chunks).toString();
+            console.log(fileString);
+
+            const finalString = fileString
+              .replace(/_TITLE_/g, req.body.title + ' - ' + req.body.artist)
+              .replace('_SOURCE_URL_', newUrl)
+              .replace('_IMAGE_URL_', req.body.coverUrl)
+              .replace('_AUDIO_URL_', req.body.audioUrl);
+
+            console.log(finalString);
+
+            const stream = Readable.from(finalString);
+            console.log('piping');
+            stream
+              .pipe(copiedFile.createWriteStream())
+              .on('error', function (err2) {
+                console.log('error');
+                console.log(err2);
+              })
+              .on('finish', function () {
+                // The file upload is complete.
+                console.log('finished');
+              });
+          });
+      });
+
+      return res.status(200).send(newUrl);
     }
 
     return res.status(200).send(file.publicUrl());
